c6ef7bf3c066a0f7a6b2dd02dc90c9c1cbba2f6c
Joaquin <joaquinpatricio2013@gmail.com>
2021-08-25 19:06:00 -0300

    cambio nombre db

--- db/create_db.sql
+++ db/create_db.sql
@@ -8,19 +8,19 @@ SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
 -- -----------------------------------------------------
--- Schema mydb
+-- Schema qrdb
 -- -----------------------------------------------------
 
 -- -----------------------------------------------------
--- Schema mydb
+-- Schema qrdb
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `mydb` DEFAULT CHARACTER SET utf8 ;
-USE `mydb` ;
+CREATE SCHEMA IF NOT EXISTS `qrdb` DEFAULT CHARACTER SET utf8 ;
+USE `qrdb` ;
 
 -- -----------------------------------------------------
--- Table `mydb`.`cursos`
+-- Table `qrdb`.`cursos`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`cursos` (
+CREATE TABLE IF NOT EXISTS `qrdb`.`cursos` (
   `nombre` VARCHAR(6) NOT NULL,
   `aclaracion` VARCHAR(45) NULL,
   PRIMARY KEY (`nombre`))
@@ -29,9 +29,9 @@ COMMENT = '	';
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`alumnos`
+-- Table `qrdb`.`alumnos`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`alumnos` (
+CREATE TABLE IF NOT EXISTS `qrdb`.`alumnos` (
   `dni` INT NOT NULL,
   `nombre` VARCHAR(45) NOT NULL,
   `apellido` VARCHAR(45) NOT NULL,
@@ -41,16 +41,16 @@ CREATE TABLE IF NOT EXISTS `mydb`.`alumnos` (
   INDEX `fk_alumnos_cursos_idx` (`nombre_curso` ASC) VISIBLE,
   CONSTRAINT `fk_alumnos_cursos`
     FOREIGN KEY (`nombre_curso`)
-    REFERENCES `mydb`.`cursos` (`nombre`)
+    REFERENCES `qrdb`.`cursos` (`nombre`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`docentes`
+-- Table `qrdb`.`docentes`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`docentes` (
+CREATE TABLE IF NOT EXISTS `qrdb`.`docentes` (
   `dni` INT NOT NULL,
   `nombre` VARCHAR(45) NOT NULL,
   `apellido` VARCHAR(45) NOT NULL,
@@ -59,9 +59,9 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`modulos`
+-- Table `qrdb`.`modulos`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`modulos` (
+CREATE TABLE IF NOT EXISTS `qrdb`.`modulos` (
   `id` INT NOT NULL,
   `dia` INT NOT NULL,
   `hora_inicio` TIME NOT NULL,
@@ -74,12 +74,12 @@ CREATE TABLE IF NOT EXISTS `mydb`.`modulos` (
   INDEX `fk_modulos_docentes1_idx` (`dni_docente` ASC) VISIBLE,
   CONSTRAINT `fk_modulos_cursos1`
     FOREIGN KEY (`nombre_curso`)
-    REFERENCES `mydb`.`cursos` (`nombre`)
+    REFERENCES `qrdb`.`cursos` (`nombre`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_modulos_docentes1`
     FOREIGN KEY (`dni_docente`)
-    REFERENCES `mydb`.`docentes` (`dni`)
+    REFERENCES `qrdb`.`docentes` (`dni`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
