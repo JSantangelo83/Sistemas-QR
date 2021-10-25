9e3bcef9c024fd37eceb4b1cbcfa698129764559
vgntzz <felenode@gmail.com>
2021-10-25 21:49:45 +0000

    test123 lol

--- qr_generation/qr.py
+++ qr_generation/qr.py
@@ -1,15 +1,14 @@
 import qrcode
 from PIL import Image
+from pathlib import Path
 
-DEFAULT_EXPORT_PATH = "./output/qr1.png"
+DEFAULT_EXPORT_DIR_PATH = Path(__file__).parent / "output"
+DEFAULT_OUTPUT_FILENAME = "output.png"
 
 def qr_generate_image(parameters):
-    '''
-    genera una imagen de qr en base a 'parameters'
-    retorna dicha imagen
-    '''
+    
     qr = qrcode.QRCode(
-        version=8,
+        version=4,
         error_correction=qrcode.constants.ERROR_CORRECT_L,
         box_size=10,
         border=4,
@@ -19,8 +18,8 @@ def qr_generate_image(parameters):
     qr.add_data(feed_data)
     return qr.make_image(fill_color="black", back_color="white")
 
-def qr_export_image(image, path=DEFAULT_EXPORT_PATH):
-    '''
+def qr_export_image(image, path=DEFAULT_EXPORT_DIR_PATH):
+    '''y
     guarda 'image' como archivo en 'path'
     '''
-    image.save(path)
+    image.save((path / DEFAULT_OUTPUT_FILENAME).resolve())
